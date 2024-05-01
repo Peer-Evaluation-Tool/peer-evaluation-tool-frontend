1d8ab7c8f2dadedbf8e5dec9acca22b6492926a6
James Nogueira <j.l.nogueira@tcu.edu>
2024-05-01 11:28:02 -0500

    Find Sections UI working

--- src/components/AdminNavigation.vue
+++ src/components/AdminNavigation.vue
@@ -3,6 +3,7 @@
         <!-- Links to admin-specific components -->
         <router-link :to="{ name: 'find-student' }">Find Student</router-link>
         <router-link :to="{ name: 'invite-students' }">Invite Students</router-link>
+        <router-link :to="{ name: 'find-sections' }">Find Sections</router-link>
         <router-link :to="{ name: 'team-management' }">Team Management</router-link>
     </div>
 </template>
